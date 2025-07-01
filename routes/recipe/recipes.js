05d6d6aaec7988fd8d53ee9094df6ed250d80860
Yon <Yon.heerkens@gmail.com>
2025-07-01 22:19:56 +0200

    remove post request for debugging

--- routes/recipe/recipes.js
+++ routes/recipe/recipes.js
@@ -9,8 +9,4 @@ router.post('/', (req, res) => {
   res.json({ message: 'This is the post endpoint for recipes' });
 });
 
-router.put(':{recipe_id}', (req, res) => {
-  res.json({ message: `This is the put endopoint ${req.recipe_id}` });
-});
-
 module.exports = router;
